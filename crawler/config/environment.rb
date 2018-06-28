db1c75c973350e82d60e97e76a255ec7e07784a8
Kevin Perez <kevin.perez@magmalabs.io>
2018-06-28 11:19:13 -0500

    add final sidekiq config
    This ensures the Sidekiq default queue is empty every time the
    crawler is restarted. It also changes the time unit used to tell
    the crawler how much time it should sleep to seconds instead of
    minutes.

--- crawler/config/environment.rb
+++ crawler/config/environment.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'byebug' unless ENV['ENVIRONMENT'] == 'production'
+
 require_relative '../models/post'
 require_relative '../models/db'
 require_relative '../models/bot'
 require_relative '../adapters/base'
 require_relative '../adapters/twitter'
 require_relative '../workers/crawler'
+
+require 'sidekiq/api'
+require 'sidekiq-scheduler'
+
+# Clean sidekiq jobs queue
+Sidekiq::Queue.new('infinity').clear
+Sidekiq::RetrySet.new.clear
+Sidekiq::ScheduledSet.new.clear
